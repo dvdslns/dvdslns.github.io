a0a3360de26f21666be91eb32fadff41a75de270
david s <messthetic@gmail.com>
2022-01-27 19:57:43 -0300

    Updates

---
+++ component---src-pages-index-js-0bd03a293bd0ff34962d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{7704:function(e,t,l){l.r(t);var a=l(7294),n=(l(5444),l(7198)),r=l(3751);t.default=function(){return a.createElement(n.Z,null,a.createElement(r.Z,{title:"dvdslns"}),a.createElement("div",null))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-0bd03a293bd0ff34962d.js.map
